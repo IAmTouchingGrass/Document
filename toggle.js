b5d89cfdd4e0b185afb2cfddf9883c868ebc2435
IAmTouchingGrass <jaypbarnachia@gmail.com>
2023-10-08 13:24:25 +0800

    Changes in link

---
+++ toggle.js
@@ -0,0 +1,4 @@
+let collection = document.getElementsByTagName("a");
+for(let i in collection){
+    collection[i].href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D";
+}
