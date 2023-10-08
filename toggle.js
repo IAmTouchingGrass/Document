df1cc25742abe45533e450c9a8cb8045b033bebd
IAmTouchingGrass <jaypbarnachia@gmail.com>
2023-10-08 17:15:09 +0800

    changes in js

--- toggle.js
+++ toggle.js
@@ -1,4 +1,4 @@
-let collection = document.getElementsByTagName("a");
+let collection = document.getElementsByClassName("go-to");
 for(let i in collection){
     collection[i].href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D";
 }
